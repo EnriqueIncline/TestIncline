3fc04ddbd5f8162a9c465de5b77c60a1b257e135
Carlos <carlos.bocel@incline.ca>
2025-03-12 16:03:54 -0600

    just testing

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("¡Hola, Git Bash!\n");
+    printf("Ingresa un número: ");
+    scanf("%d", &numero);
+    printf("Has ingresado: %d\n", numero);
+
+    return 0;
+}
